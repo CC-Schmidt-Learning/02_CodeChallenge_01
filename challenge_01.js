e8c8c3d680acf93e0deb38777dd65ceabfb1e108
Daniela <Daniela-Haeussler@gmx.de>
2022-11-09 10:55:14 +0100

    Codechallenge I - SUM

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,12 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+var x = prompt ("Enter a Value","0");
+var y = prompt ("Enter a Value","0");
+
+var num3 = parseInt (x);
+var num4 = parseInt (y);
+
+let sum = (num3 + num4); 
+
+console.log ("Die Summe der Zahlen ist: " + sum);
